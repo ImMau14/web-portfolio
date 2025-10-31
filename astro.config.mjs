db7d767db213afe7480d9109250723b13709e46b
ImMau14 <newmauuelenoski02@gmail.com>
2025-10-30 21:28:24 -0400

    Feat: Add minimal project
    With Astro, TailwindCSS, React, EsLint, Prettier

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+
+// For alias imports
+import { fileURLToPath, URL } from "node:url"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+        "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+      },
+    },
+  },
+
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+})
